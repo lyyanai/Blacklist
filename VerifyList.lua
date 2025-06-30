f009624979b47b42ee18abc56f2c26ad0d27399c
lyyanai <162587777+lyyanai@users.noreply.github.com>
2025-06-30 10:34:41 +0800

    Create VerifyList.lua

---
+++ VerifyList.lua
@@ -0,0 +1,17 @@
+local whitelist = {
+  "8a2c0502-55a4-13c1-6843-c3bf720dc82",
+  "40cb4311-2ec9-10d2-d3ca-38c4638665e3",
+  "0027e18e-900c-1d9c-6e78-173dfdd55568",
+  "69fa96f8-f770-1bff-6526-cb6b251b58cc",
+  "ca8853fa-4802-1428-6660-3d7ace2fc2f6",
+  "441741be-0cf6-171a-0c7e-0b8f50ebb519",
+  "79b558f4-afce-1c3d-df3a-0ddab58d73e0",
+  "5608c69a-3a03-19ff-047e-e03c5762595b"
+}
+local blacklist = {
+  "1fdd8c48-2bac-47bb-9215-baac6a320ded",
+  "3a1637df-13a8-407d-bef8-0634a15c1147",
+  "9e9e5961-7999-4356-b0f0-7ce459b46de1",
+  "f2cfa3e2-8eb0-4a20-9643-f21bc5427bd0"
+}
+return whitelist, blacklist
