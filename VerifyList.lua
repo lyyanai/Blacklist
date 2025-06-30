825575f1a2b3c4376d77c5b785e1b5ace02f26b5
lyyanai <162587777+lyyanai@users.noreply.github.com>
2025-06-30 11:05:01 +0800

    Update VerifyList.lua

--- VerifyList.lua
+++ VerifyList.lua
@@ -1,14 +1,7 @@
 local whitelist = {
-  "8a2c0502-55a4-13c1-6843-c3bf720dc82",
-  "40cb4311-2ec9-10d2-d3ca-38c4638665e3",
-  "0027e18e-900c-1d9c-6e78-173dfdd55568",
-  "69fa96f8-f770-1bff-6526-cb6b251b58cc",
-  "ca8853fa-4802-1428-6660-3d7ace2fc2f6",
-  "441741be-0cf6-171a-0c7e-0b8f50ebb519",
-  "79b558f4-afce-1c3d-df3a-0ddab58d73e0",
-  "5608c69a-3a03-19ff-047e-e03c5762595b"
+  "hwid"
 }
 local blacklist = {
-  "81cf138a-843c-4acf-853f-1233ac45e6ea"
+  "hwid"
 }
 return whitelist, blacklist
