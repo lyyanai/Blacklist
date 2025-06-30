83e4e946017dd29bb641a495607d4b7fc02902fc
lyyanai <162587777+lyyanai@users.noreply.github.com>
2025-06-30 11:01:16 +0800

    Update VerifyList.lua

--- VerifyList.lua
+++ VerifyList.lua
@@ -9,9 +9,6 @@ local whitelist = {
   "5608c69a-3a03-19ff-047e-e03c5762595b"
 }
 local blacklist = {
-  "1fdd8c48-2bac-47bb-9215-baac6a320ded",
-  "3a1637df-13a8-407d-bef8-0634a15c1147",
-  "9e9e5961-7999-4356-b0f0-7ce459b46de1",
-  "f2cfa3e2-8eb0-4a20-9643-f21bc5427bd0"
+  "81cf138a-843c-4acf-853f-1233ac45e6ea"
 }
 return whitelist, blacklist
